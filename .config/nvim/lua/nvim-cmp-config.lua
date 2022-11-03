f52caff590d18fd63c1fe953c3d9c41b430d6cf9
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2022-11-04 04:06:25 +0700

    Add sumneko_lua language server.

--- .config/nvim/lua/nvim-cmp-config.lua
+++ .config/nvim/lua/nvim-cmp-config.lua
@@ -145,3 +145,16 @@ lspconfig['texlab'].setup {
 	on_attach = on_attach
 }
 
+lspconfig['sumneko_lua'].setup {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			runtime = { version = 'LuaJIT' },
+			diagnostics = { globals = { 'vim' }},
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			telemetry = { enable = false }
+		}
+	}
+}
+
